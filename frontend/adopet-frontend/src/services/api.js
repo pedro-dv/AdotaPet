540230404a96a28def62ed177e9e11259780996f
pedro-dv <godofwar2906@gmail.com>
2025-05-20 09:59:53 -0300

    atualizando arquivo .env

--- frontend/adopet-frontend/src/services/api.js
+++ frontend/adopet-frontend/src/services/api.js
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+console.log("🔍 Base URL da API:", api.defaults.baseURL);
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
